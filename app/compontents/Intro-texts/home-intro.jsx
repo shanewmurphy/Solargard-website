666c7f7f9aa7b22c7f54ba163bc92b9d65df7f77
Shane Murphy <shn.murphy@gmail.com>
2024-10-03 10:20:35 +0100

    Build issue with netlify

--- app/compontents/Intro-texts/home-intro.jsx
+++ app/compontents/Intro-texts/home-intro.jsx
@@ -4,13 +4,15 @@ export default function HomeIntroText() {
       <div className="bg-secondary pt-60 pb-60 px-24">
         <h1 className="text-white font-light lg:text-3xl">
           Solar Gard Ireland Providing Innovative Glass Film Solutions for
-          Industrial, Commercial, and Domestic Applications, Solar Guard is a
-          leading provider of glass film solutions, offering a wide range of
-          products including solar films, privacy, anti-heat, anti-glare, UV
-          films, reflective films, safety films, frosted films, vehicle tinting,
-          and manifestations & graphics. We also specialise in vehicle wrapping
-          with custom graphics, and CoverStyl', a self-adhesive vinyl that
-          offers solution to enhance walls, furniture, and other surfaces.
+          Industrial&#x2C; Commercial&#x2C; and Domestic Applications&#x2C;
+          Solar Guard is a leading provider of glass film solutions&#x2C;
+          offering a wide range of products including solar films&#x2C;
+          privacy&#x2C; anti-heat&#x2C; anti-glare&#x2C; UV films&#x2C;
+          reflective films&#x2C; safety films&#x2C; frosted films&#x2C; vehicle
+          tinting, and manifestations & graphics. We also specialise in vehicle
+          wrapping with custom graphics, and CoverStyl'&#x2C; a self-adhesive
+          vinyl that offers solution to enhance walls, furniture, and other
+          surfaces.
         </h1>
       </div>
     </div>
