6b1a239a6efd0a169fa439bdb17696338965c3b7
Shane Murphy <shn.murphy@gmail.com>
2024-11-19 10:21:09 +0000

    Price list car Tinting

--- app/Vehicle-tinting/page.js
+++ app/Vehicle-tinting/page.js
@@ -1,7 +1,127 @@
 "use client";
 import { useEffect } from "react";
 import Image from "next/image";
+
 import HeroImg from "/public/images/car-tinting-imgs/Hero-car-tinting-img-min.jpg";
+import { Description } from "@headlessui/react";
+import CarTierOne from "/public/images/car-tinting-imgs/Car-imgs-pricing-A.png";
+const vehicletintingData = [
+  {
+    id: "01",
+    title: "Professional Installation: Expertise You Can Trust",
+    description:
+      "Our skilled technicians have years of experience in installing vehicle tints, guaranteeing a flawless result every time",
+    thumbnail: "/images/car-tinting-imgs/thumbnail_01.jpg",
+  },
+  {
+    id: "02",
+    title: "Long Lasting Performance: Quality That Endures",
+    description:
+      "With our high-quality tints, you can enjoy long-lasting performance and protection against harmful UV rays.",
+    thumbnail: "/images/car-tinting-imgs/thumbnail_02.jpg",
+  },
+  {
+    id: "03",
+    title: "Enhance Your Vehicle’s Style: Aesthetics That Impress",
+    description:
+      "With our high-quality tints, you can enjoy long-lasting performance and protection against harmful UV rays.",
+    thumbnail: "/images/car-tinting-imgs/thumbnail_03.jpg",
+  },
+];
+
+const tintingPricingListData = [
+  {
+    id: "01",
+    tierName: "CLASSIC",
+    filmType: "ATC Dyed",
+    image: CarTierOne,
+    fourDoorSalon: {
+      full: "Full Car",
+      half: "Half Car",
+      fullprice: 190,
+      halfprice: 150,
+    },
+    fiveDoorHatchback: {
+      full: "Full Car",
+      half: "Half Car",
+      fullprice: 190,
+      halfprice: 150,
+    },
+    estate: {
+      full: "Full Car",
+      half: "Half Car",
+      fullprice: 190,
+      halfprice: 150,
+    },
+    coupe: {
+      full: "Full Car",
+      half: "Half Car",
+      fullprice: 190,
+      halfprice: 150,
+    },
+  },
+  {
+    id: "02",
+    tierName: "HIGH PERFORMANCE",
+    filmType: "ATR Metalized",
+    image: CarTierOne,
+    fourDoorSalon: {
+      full: "Full Car",
+      half: "Half Car",
+      fullprice: 290,
+      halfprice: 250,
+    },
+    fiveDoorHatchback: {
+      full: "Full Car",
+      half: "Half Car",
+      fullprice: 290,
+      halfprice: 250,
+    },
+    estate: {
+      full: "Full Car",
+      half: "Half Car",
+      fullprice: 290,
+      halfprice: 250,
+    },
+    coupe: {
+      full: "Full Car",
+      half: "Half Car",
+      fullprice: 290,
+      halfprice: 250,
+    },
+  },
+  {
+    id: "03",
+    tierName: "AVANTGARDE",
+    filmType: "IRX Nano Ceramic",
+    image: CarTierOne,
+    fourDoorSalon: {
+      full: "Full Car",
+      half: "Half Car",
+      fullprice: 390,
+      halfprice: 250,
+    },
+    fiveDoorHatchback: {
+      full: "Full Car",
+      half: "Half Car",
+      fullprice: 390,
+      halfprice: 250,
+    },
+    estate: {
+      full: "Full Car",
+      half: "Half Car",
+      fullprice: 390,
+      halfprice: 250,
+    },
+    coupe: {
+      full: "Full Car",
+      half: "Half Car",
+      fullprice: 390,
+      halfprice: 250,
+    },
+  },
+];
+
 export default function VehicleTinting() {
   useEffect(() => {
     (async () => {
@@ -41,6 +161,110 @@ export default function VehicleTinting() {
           of shades and styles, we offer customised solutions that enhance your
           driving experience.
         </h1>
+      </div>
+      <div className="bg-grey lg:py-32 lg:px-24">
+        <div className="largeScreens:w-10/12 lg:w-11/12 mx-auto">
+          <div className="grid grid-cols-3 gap-8 antialiase">
+            {vehicletintingData.map((veheicleData) => (
+              <div
+                className="bg-white p-4 pb-24 rounded-lg text-secondary"
+                key={veheicleData.id}
+              >
+                <div className="flex relative w-full h-[390px]">
+                  <Image
+                    className="rounded-md"
+                    src={veheicleData.thumbnail}
+                    alt="Solar Gard Solar Films"
+                    sizes="100vw"
+                    placeholder="blur"
+                    blurDataURL="/path/to/placeholder.jpg" // Optional blur image
+                    fill // Automatically fills the parent container
+                    style={{
+                      objectFit: "cover", // Ensures the image covers the container
+                    }}
+                  />
+                </div>
+                <h3 className="lg:text-2xl md:text-xl font-bold mt-8">
+                  {veheicleData.title}
+                </h3>
+                <p className="mt-4">{veheicleData.description}</p>
+              </div>
+            ))}
+          </div>
+          <div className="mt-40">
+            <div className="lg:w-9/12 mx-auto antialiased">
+              <div className="lg:w-9/12 mx-auto mb-6">
+                <h3 className="text-center font-bold text-4xl">
+                  Choose the Perfect Tinting Package for Your Vehicle
+                </h3>
+                <h4 className="text-center font-medium">
+                  Below are our available packages, designed to suit different
+                  preferences and budgets. Whether you want a full vehicle tint
+                  or just a few windows, we have the right option for you.
+                </h4>
+              </div>
+            </div>
+            <div className="grid grid-cols-3 gap-12">
+              {tintingPricingListData.map((tier) => (
+                <div key={tier.id} className="bg-white rounded-lg p-8">
+                  <div className="lg:w-7/12 mx-auto">
+                    <Image src={tier.image} />
+                  </div>
+                  <h2 className="text-2xl font-bold">{tier.tierName}</h2>
+                  <p className="text-gray-600">{tier.filmType}</p>
+                  <div className="mt-6 border-b-2 pb-4">
+                    <h3 className="text-base font-semibold mb-1">
+                      4-Door Salon
+                    </h3>
+                    <div className="flex justify-between">
+                      <div>{tier.fourDoorSalon.full}</div>
+                      <div>€{tier.fourDoorSalon.fullprice}</div>
+                    </div>
+                    <div className="flex justify-between">
+                      <div>{tier.fourDoorSalon.half}</div>
+                      <div>€{tier.fourDoorSalon.halfprice}</div>
+                    </div>
+                  </div>
+                  <div className="mt-4 border-b-2 pb-4">
+                    <h3 className="text-base font-semibold mb-1">
+                      5-Door Hatchback
+                    </h3>
+                    <div className="flex justify-between">
+                      <div>{tier.fiveDoorHatchback.full}</div>
+                      <div>€{tier.fiveDoorHatchback.fullprice}</div>
+                    </div>
+                    <div className="flex justify-between">
+                      <div>{tier.fiveDoorHatchback.half}</div>
+                      <div>€{tier.fiveDoorHatchback.halfprice}</div>
+                    </div>
+                  </div>
+                  <div className="mt-4 border-b-2 pb-4">
+                    <h3 className="text-base font-semibold">Estate</h3>
+                    <div className="flex justify-between">
+                      <div>{tier.estate.full}</div>
+                      <div>€{tier.estate.fullprice}</div>
+                    </div>
+                    <div className="flex justify-between">
+                      <div>{tier.estate.half}</div>
+                      <div>€{tier.estate.halfprice}</div>
+                    </div>
+                  </div>
+                  <div className="mt-4">
+                    <h3 className="text-base font-semibold">Coupe</h3>
+                    <div className="flex justify-between">
+                      <div>{tier.coupe.full}</div>
+                      <div>€{tier.coupe.fullprice}</div>
+                    </div>
+                    <div className="flex justify-between">
+                      <div>{tier.coupe.half}</div>
+                      <div>€{tier.coupe.halfprice}</div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
