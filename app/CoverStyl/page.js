1e7b55c5727c56a73f057b107bcf57e85937f5eb
Shane Murphy <shn.murphy@gmail.com>
2024-11-28 10:30:22 +0000

    Created CoverStyl' film component

---
+++ app/CoverStyl/page.js
@@ -0,0 +1,140 @@
+"use client";
+import { useEffect } from "react";
+import Image from "next/image";
+import CoverstylFilmComponent from "../compontents/film-collections/coverastyl-films-collection";
+
+import CoverStylHeroImg from "/public/images/coverstyl/Coverstyl-hero-min.jpg";
+import CostEffective from "/public/images/coverstyl/Cost-Effective.jpg";
+import VersatileDurable from "/public/images/coverstyl/Versatile-Durable.jpg";
+import Finishes from "/public/images/coverstyl/Finishes.jpg";
+
+import Furniture from "/public/images/coverstyl/Furniture.jpg";
+import WallCoverings from "/public/images/coverstyl/Wall-Coverings.jpg";
+import commercial from "/public/images/coverstyl/Commercial-Spaces.jpg";
+
+const WhyCoverstyl = [
+  {
+    id: "01",
+    title: "Cost-Effective Renovation",
+    description:
+      "Forget the high costs of traditional refurbishments! With CoverStyl', you can completely revamp your space at a fraction of the cost. Whether it’s furniture, walls, or doors, you’ll get a premium finish without the hefty price tag.",
+    image: CostEffective,
+  },
+  {
+    id: "02",
+    title: "Versatile & Durable",
+    description:
+      "CoverStyl' is highly resistant to heat, humidity, and UV light, making it perfect for high-traffic areas and environments exposed to sunlight. Its versatility allows it to be applied to flat or curved surfaces, offering endless design possibilities.",
+    image: VersatileDurable,
+  },
+  {
+    id: "03",
+    title: "Wide Range of Finishes",
+    description:
+      "With over 500 textures and designs, there’s something to suit every taste. Choose from elegant wood grains, modern metallics, luxurious leather, or sophisticated stone effects to enhance your space’s aesthetics.",
+    image: Finishes,
+  },
+];
+
+const applications = [
+  {
+    id: "01",
+    title: "Furniture Wrapping",
+    description:
+      "Breathe new life into old furniture by wrapping it with CoverStyl'. Whether you’re looking to refresh kitchen cabinets, office desks, or hotel furnishings, CoverStyl' gives you a high-end look without the need for replacements.",
+    image: Furniture,
+  },
+  {
+    id: "02",
+    title: "Wall Coverings",
+    description:
+      "Transform blank walls into statement pieces. From luxurious wood textures to industrial-style metals, CoverStyl' can change the ambiance of a room in a matter of hours.",
+    image: WallCoverings,
+  },
+  {
+    id: "03",
+    title: "Retail & Commercial Spaces",
+    description:
+      "CoverStyl' is perfect for updating retail stores, hotels, and office spaces. Create a consistent brand look with customised finishes that match your business's aesthetic and enhance the customer experience.",
+    image: commercial,
+  },
+];
+
+export default function CoverstylePage() {
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locomotiveScroll = new LocomotiveScroll();
+    })();
+  }, []);
+  return (
+    <div className="bg-grey">
+      <div className="flex flex-col w-full">
+        <div className="relative">
+          <Image
+            src={CoverStylHeroImg}
+            alt="Solar Gard CoverStyl'"
+            sizes="100vw"
+            placeholder="blur"
+            style={{
+              width: "100%",
+              objectFit: "cover",
+            }}
+            className="h-[65vh]"
+          />
+          <div className="absolute lg:w-4/5 largeScreens:w-9/12 inset-0 flex items-end justify-start lg:p-24 md:p-12 sm:p-8 largeScreens:p-40 rounded-bl-lg">
+            {/* <h1 className="text-white lg:text-5xl md:text-3xl sm:text-xl largeScreens:text-6xl font-bold">
+                Experience the Ultimate in Comfort and Efficiency with Solar
+                Films
+              </h1> */}
+          </div>
+        </div>
+      </div>
+      <div className="bg-grey lg:py-40">
+        <h1 className="text-secondary font-medium lg:text-3xl largeScreens:text-4xl md:text-2xl sm:text-xl w-10/12 mx-auto">
+          Looking to refresh your space without the hassle of a full renovation?
+          Solar Gard Ireland offers CoverStyl', an innovative self-adhesive
+          vinyl film that transforms walls, furniture, and surfaces quickly and
+          affordably. With over 500 stunning finishes to choose from—ranging
+          from wood and marble to metallic and fabric textures—CoverStyl' brings
+          new life to your interiors while being eco-friendly and
+          cost-effective. Whether you’re renovating a commercial space or
+          updating your home’s decor, CoverStyl' is the perfect solution for a
+          sleek, modern look.
+        </h1>
+      </div>
+      <div className="lg:w-10/12 py-20 mx-auto">
+        {WhyCoverstyl.map((reason) => (
+          <div className="bg-white rounded-xl antialiased" key={reason.id}>
+            <div className="flex flex-row mb-8 p-16 gap-24">
+              <div className="basis-3/5 pt-[22%]">
+                <h2 className="font-bold text-secondary lg:text-4xl antialiased">
+                  {reason.title}
+                </h2>
+                <p className="font-medium text-textLightGray lg:text-base mt-4 antialiased">
+                  {reason.description}
+                </p>
+              </div>
+              <div className="basis-2/5">
+                <Image
+                  src={reason.image}
+                  className="rounded-2xl"
+                  alt="Solar Gard CoverStyl'"
+                  sizes="100vw"
+                  placeholder="blur"
+                  style={{
+                    width: "100%",
+                    objectFit: "cover",
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="lg:w-10/12 py-20 mx-auto">
+        <CoverstylFilmComponent />
+      </div>
+    </div>
+  );
+}
---
+++ app/compontents/film-collections/coverastyl-films-collection.jsx
@@ -0,0 +1,184 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import { Pagination } from "@nextui-org/pagination";
+
+const CoverStylData = [
+  {
+    id: "01",
+    category: "wood",
+    productName: "Rustic Oak",
+    productImage: "/images/coverstyl/coverstyl-product-img/Rustic-Oak.jpg",
+    productLink: "https://coverstyl.com/en/collection/wood/aa02/",
+  },
+  {
+    id: "02",
+    category: "wood",
+    productName: "Grey Line Oak",
+    productImage: "/images/coverstyl/coverstyl-product-img/Grey-Line-Oak.jpg",
+    productLink: "https://coverstyl.com/en/collection/wood/aa15/",
+  },
+  {
+    id: "03",
+    category: "wood",
+    productName: "Blue Ebony",
+    productImage: "/images/coverstyl/coverstyl-product-img/Blue-Ebony.jpg",
+    productLink: "https://coverstyl.com/en/collection/wood/ab05/",
+  },
+  {
+    id: "04",
+    category: "concrete",
+    productName: "Raw Grey",
+    productImage: "/images/coverstyl/coverstyl-product-img/Raw-Grey.jpg",
+    productLink: "https://coverstyl.com/en/collection/concrete/ne24/",
+  },
+  {
+    id: "05",
+    category: "concrete",
+    productName: "Cement Grey",
+    productImage: "/images/coverstyl/coverstyl-product-img/Cement-Grey.jpg",
+    productLink: "https://coverstyl.com/en/collection/concrete/u19/",
+  },
+  {
+    id: "06",
+    category: "concrete",
+    productName: "Off White Cement",
+    productImage:
+      "/images/coverstyl/coverstyl-product-img/Off-White-Cement.jpg",
+    productLink: "https://coverstyl.com/en/collection/concrete/nh33/",
+  },
+  {
+    id: "07",
+    category: "textures",
+    productName: "Mika Light",
+    productImage: "/images/coverstyl/coverstyl-product-img/Mika-Light.jpg",
+    productLink: "https://coverstyl.com/en/collection/textile/ne74/",
+  },
+  {
+    id: "08",
+    category: "textures",
+    productName: "Silver Metal Weaving",
+    productImage:
+      "/images/coverstyl/coverstyl-product-img/Silver-Metal-Weaving.jpg",
+    productLink: "https://coverstyl.com/en/collection/textile/al11/",
+  },
+  {
+    id: "09",
+    category: "textures",
+    productName: "Beige Linen",
+    productImage: "/images/coverstyl/coverstyl-product-img/Beige-Linen.jpg",
+    productLink: "https://coverstyl.com/en/collection/textile/nh19/",
+  },
+  {
+    id: "10",
+    category: "Colour",
+    productName: "Steel Blue",
+    productImage: "/images/coverstyl/coverstyl-product-img/Steel-Blue.jpg",
+    productLink: "",
+  },
+  {
+    id: "11",
+    category: "Colour",
+    productName: "Vibrant Green",
+    productImage: "/images/coverstyl/coverstyl-product-img/Vibrant-Green.jpg",
+    productLink: "https://coverstyl.com/en/collection/color/m5/",
+  },
+  {
+    id: "12",
+    category: "Colour",
+    productName: "Clean Mint",
+    productImage: "/images/coverstyl/coverstyl-product-img/Clean-Mint.jpg",
+    productLink: "https://coverstyl.com/en/collection/color/ne54/",
+  },
+];
+
+export default function CoverstylFilmComponent() {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // Adjust items per page based on screen size
+  const itemsPerPage = 4; // You can keep this fixed since we're showing all in one row
+  const totalPages = Math.ceil(CoverStylData.length / itemsPerPage);
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = CoverStylData.slice(indexOfFirstItem, indexOfLastItem);
+
+  return (
+    <div className="container mx-auto px-4">
+      <div className="bg-grey w-full">
+        <div className="lg:w-7/12 lg:mb-14 md:mb-8 sm:mb-6">
+          <h2 className="lg:text-5xl md:text-4xl sm:text-2xl lg:mb-4 md:mt-8 sm:mt-12 font-bold text-secondary">
+            Explore Our Range of CoverStyl'
+          </h2>
+          <p className="font-medium text-textGray lg:text-xl sm:mt-2">
+            Transform Your Space with Innovative Surface Design with CoverStyl’
+          </p>
+        </div>
+        {/* Added flex-nowrap and overflow-x-auto for horizontal scrolling */}
+        <div className="flex flex-nowrap overflow-x-auto gap-4 pb-4 antialiased">
+          {currentItems.map((product) => (
+            <div
+              key={product.id}
+              className="flex-none bg-white p-6 pb-16 rounded-lg shadow
+                  w-[calc(50%-8px)] sm:w-[calc(50%-8px)] 
+                  md:w-[calc(33.333%-12px)] xl:w-[calc(25%-12px)]"
+            >
+              <div className="h-52 w-52 mx-auto mb-4">
+                <img
+                  src={product.productImage}
+                  alt={product.productName}
+                  className="rounded-full h-52 w-52"
+                />
+              </div>
+
+              <h3 className="font-bold text-center text-secondary lg:text-xl">
+                {product.productName}
+              </h3>
+              <p className="text-center font-medium text-textLightGray">
+                {product.category}
+              </p>
+              <div className="text-center mt-4 antialiased">
+                <button
+                  type="button"
+                  onClick={() =>
+                    window.open(
+                      product.productLink,
+                      "_blank",
+                      "noopener,noreferrer"
+                    )
+                  }
+                  className="outline outline-offset-2 outline-1 outline-slate-900 lg:text-sm md:text-sm sm:text-xs text-secondary font-semibold rounded-sm py-1 px-4 hover:bg-slate-900 hover:text-white"
+                >
+                  View Specs
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="flex justify-center mt-8 mb-8">
+          <Pagination
+            color="secondary"
+            total={totalPages}
+            initialPage={1}
+            onChange={(page) => setCurrentPage(page)}
+          />
+        </div>
+        <div className="mx-auto">
+          <h5 className="text-center text-secondary font-semibold lg:text-xl">
+            Please visit our CoverStyl' Partner for full list of materials
+          </h5>
+          <div className="text-center mt-4">
+            <Link href="https://coverstyl.com/en/" target="_blank">
+              <button
+                type="button"
+                className="outline outline-offset-2 outline-1 outline-slate-900 lg:text-base md:text-sm sm:text-xs text-secondary font-semibold rounded-sm py-1 px-8 hover:bg-slate-900 hover:text-white"
+              >
+                Our Partner
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
